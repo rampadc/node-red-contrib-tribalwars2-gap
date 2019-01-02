a6bc342f2712588d25e1b176189701cc9d4db578
Cong <rampadc@users.noreply.github.com>
2019-01-02 18:56:10 +1100

    Properly build for production to slim down releases

--- setup.sh
+++ setup.sh
@@ -2,16 +2,23 @@
 
 # Update node-red
 rm -rf node-red
+rm -rf node-red-src
 git submodule init
 git submodule update
+mv node-red node-red-src
 
 # Replace nodes with custom
-rm -rf node-red/nodes/*
-cp -r nodes node-red/nodes
-rm node-red/settings.js
-cp settings.js node-red/settings.js
+rm -rf node-red-src/nodes/*
+cp -r nodes node-red-src/nodes
+rm node-red-src/settings.js
+cp settings.js node-red-src/settings.js
 
 # Run and build node-red: https://github.com/node-red/node-red
-cd node-red
+cd node-red-src
 npm install
-npm run build
+grunt release
+
+cp -a .dist/node-red-*/. ../node-red
+cd ../node-red
+rm -rf ../node-red-src
+npm install --production

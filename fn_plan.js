416e45aa91e21c47f522ea320a6493b559aa0219
Cong <rampadc@users.noreply.github.com>
2019-06-03 01:12:33 +1000

    Fixing frozen bug

--- fn_plan.js
+++ fn_plan.js
@@ -7,35 +7,38 @@ module.exports = function (RED) {
         this.on('input', (msg) => {
             this.status({});
 
-            // couple of status notices
-            if (msg.payload.hasOwnProperty('commandType')) {
-                if (msg.payload['commandType'] === 'attack') {
-                    this.status({ fill: "red", shape: "ring", text: "Attack" });
-                } else if (msg.payload['commandType'] === 'support') {
-                    this.status({ fill: "green", shape: "ring", text: "Support" });
+            if (msg.hasOwnProperty['payload']) {
+                // couple of status notices
+                if (msg.payload.hasOwnProperty('commandType')) {
+                    if (msg.payload['commandType'] === 'attack') {
+                        this.status({ fill: "red", shape: "ring", text: "Attack" });
+                    } else if (msg.payload['commandType'] === 'support') {
+                        this.status({ fill: "green", shape: "ring", text: "Support" });
+                    }
                 }
-            }
 
-            if (msg.payload.hasOwnProperty('clearing')) {
-                if (msg.payload['clearing'] === true) {
-                    this.status({ fill: "red", shape: "dot", text: "Clearing nuke" });
+                if (msg.payload.hasOwnProperty('clearing')) {
+                    if (msg.payload['clearing'] === true) {
+                        this.status({ fill: "red", shape: "dot", text: "Clearing nuke" });
+                    }
                 }
-            }
 
-            if (msg.payload.hasOwnProperty('slowestUnit')) {
-                if (msg.payload['slowestUnit'] === 'noble') {
-                    this.status({ fill: "blue", shape: "dot", text: "Noble" });
+                if (msg.payload.hasOwnProperty('slowestUnit')) {
+                    if (msg.payload['slowestUnit'] === 'noble') {
+                        this.status({ fill: "blue", shape: "dot", text: "Noble" });
+                    }
                 }
-            }
 
-            let launchDate = getLaunchDate(
-                msg.payload.source, msg.payload.target,
-                msg.payload.landingTime,
-                msg.payload.commandType, msg.payload.slowestUnit, msg.payload.worldSpeed,
-                msg.payload.attackModifiers, msg.payload.supportModifiers,
-                msg.payload.isClearingNuke
-            );
-            msg.payload['launchDate'] = launchDate;
+                let launchDate = getLaunchDate(
+                    msg.payload.source, msg.payload.target,
+                    msg.payload.landingTime,
+                    msg.payload.commandType, msg.payload.slowestUnit, msg.payload.worldSpeed,
+                    msg.payload.attackModifiers, msg.payload.supportModifiers,
+                    msg.payload.isClearingNuke
+                );
+                msg.payload['launchDate'] = launchDate;
+            }
+        
             this.send(msg);
         });
     }
